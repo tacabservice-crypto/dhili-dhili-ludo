fd91d123e7678e67f1bdc7642641899f979157fc
dhilidhili <tacabservice@gmail.com>
2026-08-02 19:46:53 +0300

    Fixed login button styling

--- server.ts
+++ server.ts
@@ -42,7 +42,6 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
